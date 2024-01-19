dc6370f3c1d7b4d358588877be7672af2ac29698
not-celeska <bushkov.mikhail@gmail.com>
2024-01-18 22:54:05 -0500

    commented the StatsActivity

--- app/src/main/java/com/example/uitester/StatsActivity.java
+++ app/src/main/java/com/example/uitester/StatsActivity.java
@@ -1,8 +1,9 @@
+// == FILE LOCATION =========
 package com.example.uitester;
 
+// == IMPORTS ==================================
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.WindowCompat;
-
 import android.app.Dialog;
 import android.os.Bundle;
 import android.os.Handler;
@@ -10,30 +11,133 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.TextView;
-
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+// == STATS MENU =====================================
 public class StatsActivity extends AppCompatActivity {
 
+    // [FIELD] File address for the statistics file.
     private final String STAT_FILE_ADDRESS = "stats.txt";
 
-    // all the text areas
+    // [FIELD] All the text areas which display the stats.
     TextView totalPlayTimeDisplay, totalGuessesMadeDisplay, totalGamesPlayedDisplay;
     TextView highestScoreDisplay, averageGuessesDisplay, averageTimeDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // [LAYOUT] Default layout creation.
         super.onCreate(savedInstanceState);
-        clearSystemUI();
         setContentView(R.layout.activity_stats);
+        clearSystemUI();
+
+        // [DISPLAYS] Wire every TextView to a variable.
         setupDisplays();
+
     }
 
+
+    // ==================================
+    // == MENU BUTTON FUNCTIONS =========
+    // ==================================
+
+
+    // Parameters: View | Requirement for button-activation.
+    // Description: Opens popup, resets text in stat file, redraws TextViews.
+    public void resetStats(View view) {
+
+        ImageButton resetButton = findViewById(R.id.resetButton);
+        resetButton.setImageResource(R.drawable.reset_pressed);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                resetButton.setImageResource(R.drawable.reset);
+
+                // [POPUP] Begins construction of the confirmation popup.
+                Dialog confirmationScreen = new Dialog(StatsActivity.this);
+                confirmationScreen.setContentView(R.layout.confirmation_screen);
+                confirmationScreen.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                confirmationScreen.setCancelable(false);
+
+                // [POPUP-NO] Creates the no button on the popup screen.
+                ImageButton noButton = confirmationScreen.findViewById(R.id.noButton);
+                noButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        noButton.setImageResource(R.drawable.no_pressed);
+                        new Handler().postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                noButton.setImageResource(R.drawable.no);
+
+                                confirmationScreen.cancel(); // [CLARITY] Closes the popup.
+
+                            }
+                        }, 80);
+                    }
+                });
+
+                // [POPUP-YES] Creates the yes button on the popup screen.
+                ImageButton yesButton = confirmationScreen.findViewById(R.id.yesButton);
+                yesButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        yesButton.setImageResource(R.drawable.yes_pressed);
+                        new Handler().postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                noButton.setImageResource(R.drawable.yes);
+
+                                clearStatFile(); // [CLARITY] Clears stat file.
+                                updateDisplays(); // [CLARITY] "Redraws" the shown stats.
+                                confirmationScreen.cancel(); // [CLARITY] Closes the popup.
+
+                            }
+                        }, 80); // Delay to see the icon change
+                    }
+                });
+
+                confirmationScreen.show(); // [CLARITY] Actually shows the popup.
+
+            }
+        }, 80);
+
+    }
+
+    // Parameters: View | Requirement for button-activation.
+    // Description: Quickly switches the gotcha button image closes the menu.
+    public void closeMenu(View view) {
+
+        ImageButton gotchaButton = findViewById(R.id.gotchaButton);
+        gotchaButton.setImageResource(R.drawable.gotcha_pressed);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                gotchaButton.setImageResource(R.drawable.gotcha);
+
+                finish(); // [CLARITY] This just closes this menu screen.
+
+            }
+        }, 80);
+    }
+
+
+    // ==================================
+    // == BACK-END OPERATION FUNCTIONS ==
+    // ==================================
+
+
+    // Parameters: None | Uses static variable access.
+    // Description: Sets up the TextView variables with their UI counterparts.
     private void setupDisplays() {
+
+        // [CLARITY] Initializes every TextView variable.
         totalPlayTimeDisplay = findViewById(R.id.totalPlayTimeDisplay);
         totalGuessesMadeDisplay = findViewById(R.id.totalGuessesMadeDisplay);
         totalGamesPlayedDisplay = findViewById(R.id.totalGamesPlayedDisplay);
@@ -41,26 +145,40 @@ public class StatsActivity extends AppCompatActivity {
         averageGuessesDisplay = findViewById(R.id.averageGuessesDisplay);
         averageTimeDisplay = findViewById(R.id.averageTimeDisplay);
 
-        updateDisplays();
+        updateDisplays(); // [CLARITY] After initializing, show the stats.
+
     }
 
+    // Parameters: None | Uses static variable access.
+    // Description: Updates the TextViews to show the stats.
     private void updateDisplays() {
-        String[] rawData = readStatFile().split(" ");
+
+        // [CLARITY] Reads the file.
+        String[] rawData = statFileEmpty().split(" ");
+
+        // [CLARITY] Formats and sets the text of the TextViews.
         totalPlayTimeDisplay.setText(formatTime(rawData[0]));
         totalGuessesMadeDisplay.setText(formatBasicStat(rawData[1]));
         totalGamesPlayedDisplay.setText(formatBasicStat(rawData[2]));
         highestScoreDisplay.setText(formatScore(rawData[3]));
         averageGuessesDisplay.setText(formatBasicStat(rawData[4]));
+
+        // [CLARITY] .replace() needed because of how file writing works.
         averageTimeDisplay.setText(formatTime(rawData[5].replace("\n", "")));
+
     }
 
+    // Parameters: String | Time in MM:SS format.
+    // Description: Converts MM:SS to MMm SSs.
     private String formatTime(String unformattedTime) {
-        // expected in MM:SS
+
         String formattedTime = "";
 
+        // [CLARITY] Extract the given info.
         int minutes = Integer.parseInt((unformattedTime.split(":"))[0]);
         int seconds = Integer.parseInt((unformattedTime.split(":"))[1]);
 
+        // [MINUTES] Format minutes.
         if (minutes < 10) {
             formattedTime += ("0" + minutes);
         }
@@ -70,6 +188,7 @@ public class StatsActivity extends AppCompatActivity {
 
         formattedTime += "m ";
 
+        // [SECONDS] Format seconds.
         if (seconds < 10) {
             formattedTime += ("0" + seconds);
         }
@@ -79,18 +198,26 @@ public class StatsActivity extends AppCompatActivity {
 
         formattedTime += "s";
 
+        // [RETURN] Return the final formatted time.
         return formattedTime;
+
     }
 
+    // Parameters: String | One or two digit number.
+    // Description: Converts to two digit sequence: ex. 1 --> 01.
     private String formatBasicStat(String unformattedTwoDigitNumber) {
+
         if (Integer.parseInt(unformattedTwoDigitNumber) < 10) {
             return "0" + unformattedTwoDigitNumber;
         }
         else {
             return unformattedTwoDigitNumber;
         }
+
     }
 
+    // Parameters: String | Some digit number.
+    // Description: Converts to four digit sequence: ex. 123 --> 0123.
     private String formatScore(String unformattedScore) {
 
         if (Integer.parseInt(unformattedScore) < 10) {
@@ -105,14 +232,17 @@ public class StatsActivity extends AppCompatActivity {
         else {
             return unformattedScore;
         }
+
     }
 
+    // Parameters: None | Actions *inside* affect the UI construction.
+    // Description: Gets rid of system UI: Navigation & Status bars.
     private void clearSystemUI() {
 
-        // Gets rid of the status bar / makes it transparent
+        // [CLARITY] Gets rid of the top status bar.
         WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
 
-        // Gets rid of the nav bar
+        // [CLARITY] Gets rid of the bottom navigation bar.
         View decorView = getWindow().getDecorView();
         decorView.setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
@@ -122,86 +252,25 @@ public class StatsActivity extends AppCompatActivity {
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
     }
 
-    public void resetStats(View view) {
-        ImageButton resetButton = findViewById(R.id.resetButton);
-        resetButton.setImageResource(R.drawable.reset_pressed);
+    // Parameters: None | Uses static variable access.
+    // Description: Resets the stat file with all 0 values.
+    private void clearStatFile() {
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
+        // [NOTE] I used a video tutorial for the file IO; not 100% sure of the processes used.
 
-
-                resetButton.setImageResource(R.drawable.reset);
-
-
-                Dialog confirmationScreen = new Dialog(StatsActivity.this);
-                confirmationScreen.setContentView(R.layout.confirmation_screen);
-                confirmationScreen.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-                confirmationScreen.setCancelable(false);
-
-                ImageButton noButton = confirmationScreen.findViewById(R.id.noButton);
-                noButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        noButton.setImageResource(R.drawable.no_pressed);
-
-                        new Handler().postDelayed(new Runnable() {
-                            @Override
-                            public void run() {
-                                noButton.setImageResource(R.drawable.no);
-                                confirmationScreen.cancel();
-                            }
-                        }, 80); // Delay to see the icon change
-                    }
-                });
-
-                ImageButton yesButton = confirmationScreen.findViewById(R.id.yesButton);
-                yesButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        yesButton.setImageResource(R.drawable.yes_pressed);
-
-                        new Handler().postDelayed(new Runnable() {
-                            @Override
-                            public void run() {
-                                noButton.setImageResource(R.drawable.yes);
-                                saveToStatFile("0:0 0 0 0 0 0:0");
-                                updateDisplays();
-                                confirmationScreen.cancel();
-                            }
-                        }, 80); // Delay to see the icon change
-                    }
-                });
-
-                confirmationScreen.show();
-            }
-        }, 80); // Delay to see the icon change
-    }
-
-    public void closeMenu(View view) {
-        ImageButton gotchaButton = findViewById(R.id.gotchaButtonStat);
-        gotchaButton.setImageResource(R.drawable.gotcha_pressed);
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                gotchaButton.setImageResource(R.drawable.gotcha);
-                finish();
-            }
-        }, 80); // Delay to see the icon change
-    }
-
-    public void saveToStatFile(String textToSave) {
         FileOutputStream fileOutputStream = null;
 
         try {
+            // [CLARITY] Initialize fileOutputStream to write the stat file.
             fileOutputStream = openFileOutput(STAT_FILE_ADDRESS, MODE_PRIVATE);
-            fileOutputStream.write(textToSave.getBytes());
+            fileOutputStream.write("0:0 0 0 0 0 0:0".getBytes());
         }
         catch (Exception e) {
             e.printStackTrace();
         }
         finally {
+
+            // [CLARITY] If fileOutputStream was opened: close it.
             if (fileOutputStream != null) {
                 try {
                     fileOutputStream.close();
@@ -209,24 +278,33 @@ public class StatsActivity extends AppCompatActivity {
                     e.printStackTrace();
                 }
             }
+
         }
     }
 
-    public String readStatFile() {
+    // Parameters: None | Uses static variable access.
+    // Description: Returns a string; what was read from the stat file.
+    private String statFileEmpty() {
+
+        // [NOTE] I used a video tutorial for the file IO; not 100% sure of the processes used.
 
         FileInputStream fileInputStream = null;
 
         try {
+
+            // [CLARITY] Creates objects necessary to read the file.
             fileInputStream = openFileInput(STAT_FILE_ADDRESS);
             InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
             BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
             StringBuilder stringBuilder = new StringBuilder();
             String textToLoad;
 
+            // [CLARITY] Reads the text in the file; stores in a String.
             while ((textToLoad = bufferedReader.readLine()) != null) {
                 stringBuilder.append(textToLoad).append("\n");
             }
 
+            // [CLARITY] Will return whatever was inside the file.
             return stringBuilder.toString();
 
         }
@@ -234,6 +312,8 @@ public class StatsActivity extends AppCompatActivity {
             e.printStackTrace();
         }
         finally {
+
+            // [CLARITY] If fileInputStream was opened: close it.
             if (fileInputStream != null) {
                 try {
                     fileInputStream.close();
@@ -242,6 +322,7 @@ public class StatsActivity extends AppCompatActivity {
                     e.printStackTrace();
                 }
             }
+
         }
 
         return "FAIL";
