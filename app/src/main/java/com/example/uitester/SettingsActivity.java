cc73e11d2dbc426ce84650f1b15b484ba3cb60d4
not-celeska <bushkov.mikhail@gmail.com>
2024-01-18 22:23:11 -0500

    commented the SettingsActivity

--- app/src/main/java/com/example/uitester/SettingsActivity.java
+++ app/src/main/java/com/example/uitester/SettingsActivity.java
@@ -1,8 +1,10 @@
+// == FILE LOCATION =========
 package com.example.uitester;
+
+// == IMPORTS ==================================
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.WindowCompat;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -12,39 +14,109 @@ import android.widget.ImageButton;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+// == SETTINGS MENU =====================================
 public class SettingsActivity extends AppCompatActivity {
 
     TextView numDigitsDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // [LAYOUT] Default layout creation.
         super.onCreate(savedInstanceState);
-
-        clearSystemUI();
-
         setContentView(R.layout.activity_settings);
+        clearSystemUI();
 
         // [VIEWS] This is the display which says how many digits there are.
         numDigitsDisplay = findViewById(R.id.numDigitsDisplay);
 
         // [VIEWS] Initialize and configure slider.
+        setupSlider();
 
-        // Declare and initialize slider.
+    }
+
+
+    // ==================================
+    // == MENU BUTTON FUNCTIONS =========
+    // ==================================
+
+
+    // Parameters: View | Requirement for button-activation.
+    // Description: Quickly switches the gotcha button image, closes the menu.
+    public void closeMenu(View view) {
+
+        ImageButton backButton = findViewById(R.id.backButton);
+        backButton.setImageResource(R.drawable.back_pressed);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                backButton.setImageResource(R.drawable.back);
+
+                finish(); // [CLARITY] This just closes this menu screen.
+
+            }
+        }, 80);
+    }
+
+    // Parameters: View | Requirement for button-activation.
+    // Description: Quickly switches the confirm button image, passes settings into next screen and starts gameplay.
+    public void startGame(View view) {
+
+        ImageButton confirmButton = findViewById(R.id.confirmButton);
+        confirmButton.setImageResource(R.drawable.confirm_pressed);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                confirmButton.setImageResource(R.drawable.confirm);
+
+                // [CLARITY] Sets up the next screen.
+                Intent intent = new Intent(SettingsActivity.this, GameplayActivity.class);
+
+                // [EXTRA] Packages the selected number of digits selected by the user.
+                intent.putExtra("numDigits", Integer.valueOf(numDigitsDisplay.getText().toString()));
+
+                // [EXTRA] Packages whether the user wants the correct number to be shown.
+                CheckBox showCorrectNumberToggle = findViewById(R.id.showCorrectNumberToggle);
+                intent.putExtra("showCorrectNumber", showCorrectNumberToggle.isChecked());
+
+                // [EXTRA] Packages user's "don't save score" status.
+                CheckBox dontSaveScoreToggle = findViewById(R.id.dontSaveScoreToggle);
+                intent.putExtra("dontSaveScore", dontSaveScoreToggle.isChecked());
+
+                // [CLARITY] Opens the gameplay menu screen.
+                startActivity(intent);
+
+            }
+        }, 80);
+    }
+
+
+    // ==================================
+    // == BACK-END OPERATION FUNCTIONS ==
+    // ==================================
+
+
+    // Parameters: None | Actions *inside* affect the UI construction.
+    // Description: Creates the mechanics behind the slider.
+    private void setupSlider() {
+
+        // [CLARITY] Declare and initialize slider.
         SeekBar numDigitsSlider = findViewById(R.id.numDigitsSlider);
 
-        // Set progress, maximum and minimum value.
+        // [CLARITY] Set progress increment; how many ticks can change at once.
         numDigitsSlider.setKeyProgressIncrement(1);
 
-        // Setup logic for when the slider is changed.
-        numDigitsSlider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
-        {
+        // [CLARITY] Setup logic for when the slider is changed.
+        numDigitsSlider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 
-                // Determine the current value at which the slider is at.
+                // [CLARITY] Determine the current value at which the slider is at.
                 int alignedProgress = Math.round((float) progress);
 
-                // Update the digits display.
+                // [CLARITY] Update the digits display.
                 numDigitsDisplay.setText(String.valueOf(alignedProgress - 1));
 
             }
@@ -52,7 +124,6 @@ public class SettingsActivity extends AppCompatActivity {
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
                 // [CLARITY] This is just an implemented function; it isn't used.
-                // TODO: Add colours?
             }
 
             @Override
@@ -63,46 +134,14 @@ public class SettingsActivity extends AppCompatActivity {
 
     }
 
-    public void closeMenu(View view) {
-        ImageButton backButton = findViewById(R.id.backButton);
-        backButton.setImageResource(R.drawable.back_pressed);
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                backButton.setImageResource(R.drawable.back);
-                finish();
-            }
-        }, 80); // Delay to see the icon change
-    }
-
-    public void startGame(View view) {
-        ImageButton confirmButton = findViewById(R.id.confirmButton);
-        confirmButton.setImageResource(R.drawable.confirm_pressed);
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                confirmButton.setImageResource(R.drawable.confirm);
-                Intent intent = new Intent(SettingsActivity.this, GameplayActivity.class);
-                intent.putExtra("numDigits", Integer.valueOf(numDigitsDisplay.getText().toString()));
-                CheckBox showCorrectNumberToggle = findViewById(R.id.showCorrectNumberToggle);
-                intent.putExtra("showCorrectNumber", showCorrectNumberToggle.isChecked());
-                CheckBox dontSaveScoreToggle = findViewById(R.id.dontSaveScoreToggle);
-                intent.putExtra("dontSaveScore", dontSaveScoreToggle.isChecked());
-                startActivity(intent);
-            }
-        }, 80); // Delay to see the icon change
-    }
-
-
-
+    // Parameters: None | Actions *inside* affect the UI construction.
+    // Description: Gets rid of system UI: Navigation & Status bars.
     private void clearSystemUI() {
 
-        // Gets rid of the status bar / makes it transparent
+        // [CLARITY] Gets rid of the top status bar.
         WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
 
-        // Gets rid of the nav bar
+        // [CLARITY] Gets rid of the bottom navigation bar.
         View decorView = getWindow().getDecorView();
         decorView.setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
@@ -110,6 +149,7 @@ public class SettingsActivity extends AppCompatActivity {
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
     }
 
 }
